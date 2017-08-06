537987a5aba7fc1eec04f0ce3e1735be9c9e6703
galbrecht18 <galbrecht18@gmail.com>
2017-08-06 11:09:06 -0400

    Change root to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #add basic html so we can set the home page to this and thus we can avoid error on heroku
-  def hello
-    render html: "Hello world!"
-  end
-
 end
