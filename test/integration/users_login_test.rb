930d1f4430d639ebb501e28e1dae8a062e73db4d
galbrecht18 <galbrecht18@gmail.com>
2017-10-04 23:04:38 -0400

    Add jquery to gemfile, integration test for logging in (valid login)

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,6 +4,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+  end
   test "login with invalid information" do
     get login_path # go to the login page
     assert_template 'sessions/new' # assert that we are seeing the new / login page
@@ -12,5 +15,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty? # assert we have a flash message
     get root_path # visit the home page
     assert flash.empty? # assert that we aren't seeing the flash message
+  end
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: {email: @user.email, password: 'password'}}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0 # there are no login links on the page
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
